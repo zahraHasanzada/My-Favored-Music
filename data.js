a9d0a663d78fb984afe7fcefe72af65b29d4961f
zahraHasanzada <116168664+zahraHasanzada@users.noreply.github.com>
2024-07-23 14:29:03 -0600

    Add files via upload

---
+++ data.js
@@ -0,0 +1,38 @@
+let songs =[
+    {
+        name: 'Mikhay-Beraghsi',
+        path:'song/Ahmad Saeedi - Mikhay-Beraghsi (320).mp3',
+        artist : 'Ahmad Saeedi ',
+        cover:'imag/AhmadSaeedi.jpg'
+
+    },
+    {
+        name: 'Khab',
+        path:'song/Amir Mahan Khab.mp3',
+        artist : 'Amir Mahan',
+        cover:'imag/AmirMahan.jpg'
+
+    },
+    {
+        name: 'Atre Bahar',
+        path:'song/Aron Afshar - Atre Bahar (320).mp3',
+        artist : 'Aron Afshar ',
+        cover:'imag/AronAfshar.jpg'
+
+    },
+    {
+        name: 'To Shodi Mosken Dardam',
+        path:'song/Hayedeh - To Shodi Mesle Khoon Too Ragam (320) (1).mp3',
+        artist : 'Hayedeh',
+        cover:'imag/haida.jpg'
+
+    },
+    {
+        name: 'Madar',
+        path:'song/Ragheb - Madar.mp3',
+        artist : ' Ragheb',
+        cover:'imag/Raqheb.jpg'
+
+    }
+
+]
